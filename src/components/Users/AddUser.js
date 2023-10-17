51117c5f77eff3cba36da0f11d83ecece92c024b
mila2999 <mila.savonik@gmail.com>
2023-10-17 19:35:40 +0300

    Added  User Conponent and re-usable Card Componenti

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const addUserHendler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHendler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" />
+
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+export default AddUser;
